a4014c8ca1aaa258953c88eb05ada6d433498854
Raymond Jacobson <ray@audius.co>
2026-01-30 23:23:59 -0700

    Try again

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,7 +4,4 @@ import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cac
 
 export default defineCloudflareConfig({
 	incrementalCache: r2IncrementalCache,
-	// Configure edge externals - only node:crypto should be external
-	// This ensures @libsql/client is bundled
-	edgeExternals: ["node:crypto"],
 });
